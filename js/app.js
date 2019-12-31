670c02fa1fda84cd14be8cdecab75efca0c48c95
ninjahatodi1 <45872727+ninjahatodi1@users.noreply.github.com>
2019-12-31 22:11:36 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const h1 = document.getElementById('h1');
+const changeBtn = document.getElementById('changeBtn');
+
+
+changeBtn.addEventListener('touchstart', showQuote);
+changeBtn.addEventListener('click', showQuote);
+
+function showQuote() {
+  fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+      // animate(h1, 'flipInY');
+      h1.innerText = data.content;
+    });
+}
+
+
+// function animate(element, animation) {
+//   element.classList.add(animation);
+//   const wait = setTimeout(() => {
+//     element.classList.remove(animation);
+//   }, 1000);
+// }
